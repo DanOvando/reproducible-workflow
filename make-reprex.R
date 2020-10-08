c854ea22a81a702b0492a7130f17814f14eed7ad
DanOvando <danovan@uw.edu>
2020-10-08 09:42:17 -0700

    testing gh-pages

--- make-reprex.R
+++ make-reprex.R
@@ -1,31 +1,35 @@
 
 
+
 # Make Hake Reprex -------------------------------------------------------------
 # A script to demonstrate a reproducible workflow
 
 
 # setup -------------------------------------------------------------------
-library(sraplus)
-library(janitor)
-library(here)
+library(sraplus) # package to fit surplus production model
+library(janitor) # clean up names
+library(here) # find all the things
 
 results_name <- "v1.0" # specify the name of the saved results
 
-results_path <- here("results", results_name) # create path to results
+results_path <-
+  here("results", results_name) # create path to results
 
-if (!dir.exists(results_path)) { # create directory if it's missing
+if (!dir.exists(results_path)) {
+  # create results directory if it's missing
   dir.create(results_path, recursive = TRUE)
 }
 
-hake_shape <- 2 #shape parameter in pella-tomlinson function
+hake_shape <- 3 #shape parameter in pella-tomlinson function
 
 # load functions
 
 funs <- list.files(here("functions")) # find all functions
 
-sapply(funs, function(x) source(here("functions",x)))
+sapply(funs, function(x)
+  source(here("functions", x)))
 
-hello_world() # hi. 
+hello_world() # hi.
 
 # load data ---------------------------------------------------------------
 
@@ -33,7 +37,8 @@ hake_data <- read.csv(here("data", "hake.csv")) # load data
 
 head(hake_data)
 
-hake_data <- janitor::clean_names(hake_data) # clean up names - leave raw data as is!
+hake_data <-
+  janitor::clean_names(hake_data) # clean up names - leave raw data as is!
 
 head(hake_data)
 
@@ -41,7 +46,7 @@ head(hake_data)
 # fit model ---------------------------------------------------------------
 
 # format data and priors
-
+# see github.com/danovando/sraplus
 
 hake_driors <- sraplus::format_driors(
   taxa = "Merluccius capensis",
@@ -54,6 +59,7 @@ hake_driors <- sraplus::format_driors(
 
 drior_plot <- plot_driors(hake_driors)
 
+# fit surplus production model to hake abundance index
 hake_fit <-
   sraplus::fit_sraplus(
     hake_driors,
@@ -62,16 +68,18 @@ hake_fit <-
     estimate_initial_state = FALSE,
   )
 
-fit_plot <- plot_sraplus(hake_fit)
+fit_plot <- sraplus::plot_sraplus(hake_fit)
 
-pp_plot <- plot_prior_posterior(hake_fit, hake_driors)
+pp_plot <- sraplus::plot_prior_posterior(hake_fit, hake_driors)
 
-hake_results <- summarize_sralpus(hake_fit)
-
+hake_results <- sraplus::summarize_sralpus(hake_fit)
 
 # save results ------------------------------------------------------------
 
-plots <- ls()[grepl("_plot", ls())] # find all things that end in _plot
+plots <-
+  ls()[grepl("_plot", ls())] # find all things that end in _plot
+
+# just a little trick. Combining file.path with the output of here keeps the formatting consistent
 
 save(file = file.path(results_path, "hake-plots.Rdata"),
      list = plots) # save plots
@@ -82,12 +90,16 @@ saveRDS(hake_results, file = file.path(results_path, "hake-results.rds")) # save
 # knit report -------------------------------------------------------------
 
 # specify output formats for reports
-output_format = c("bookdown::html_document2", "bookdown::pdf_document2","bookdown::word_document2")
+output_format = c(
+  "bookdown::html_document2",
+  "bookdown::pdf_document2",
+  "bookdown::word_document2"
+)
 
 # render reports
 rmarkdown::render(
   here::here("documents", "hake-report.Rmd"),
   params = list(results_name = results_name),
   output_format = output_format,
-  output_dir = results_path
+  output_dir = results_path # put the reports in the results directory
 )
