2ff18be9fe76c935001ef45e3052cd960f6b0a90
DanOvando <danovan@uw.edu>
2020-10-07 22:13:22 -0700

    comments

--- make-reprex.R
+++ make-reprex.R
@@ -9,31 +9,31 @@ library(sraplus)
 library(janitor)
 library(here)
 
-results_name <- "v1.0"
+results_name <- "v1.0" # specify the name of the saved results
 
-results_path <- here("results", results_name)
+results_path <- here("results", results_name) # create path to results
 
-if (!dir.exists(results_path)) {
-  dir.create(results_path)
+if (!dir.exists(results_path)) { # create directory if it's missing
+  dir.create(results_path, recursive = TRUE)
 }
 
 hake_shape <- 2 #shape parameter in pella-tomlinson function
 
 # load functions
 
-funs <- list.files(here("functions"))
+funs <- list.files(here("functions")) # find all functions
 
 sapply(funs, function(x) source(here("functions",x)))
 
-hello_world()
+hello_world() # hi. 
 
 # load data ---------------------------------------------------------------
 
-hake_data <- read.csv(here("data", "hake.csv"))
+hake_data <- read.csv(here("data", "hake.csv")) # load data
 
 head(hake_data)
 
-hake_data <- janitor::clean_names(hake_data)
+hake_data <- janitor::clean_names(hake_data) # clean up names - leave raw data as is!
 
 head(hake_data)
 
@@ -71,18 +71,20 @@ hake_results <- summarize_sralpus(hake_fit)
 
 # save results ------------------------------------------------------------
 
-plots <- ls()[grepl("_plot", ls())]
+plots <- ls()[grepl("_plot", ls())] # find all things that end in _plot
 
 save(file = file.path(results_path, "hake-plots.Rdata"),
-     list = plots)
+     list = plots) # save plots
 
-saveRDS(hake_results, file = file.path(results_path, "hake-results.rds"))
+saveRDS(hake_results, file = file.path(results_path, "hake-results.rds")) # save specific objects
 
 
 # knit report -------------------------------------------------------------
 
+# specify output formats for reports
 output_format = c("bookdown::html_document2", "bookdown::pdf_document2","bookdown::word_document2")
 
+# render reports
 rmarkdown::render(
   here::here("documents", "hake-report.Rmd"),
   params = list(results_name = results_name),
